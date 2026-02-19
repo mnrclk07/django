49c5394026067996e05a2598baacab6f5a5851a3
mnrclk07 <mnrclk21@gmail.com>
2026-02-19 20:59:30 +0300

    eighth commit

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+
+
+admin.site.register(Movie)
+
+# ? sonrasında py manage.py makemigrations komutu ile modelimizde yaptığımız değişiklikleri algılayarak bir migration dosyası oluşturuyoruz. Bu dosya, veritabanında yapılacak değişiklikleri tanımlar. Eğer modelinizde yeni bir alan eklediyseniz veya mevcut bir alanı değiştirdiyseniz, bu komutu çalıştırarak Django'ya bu değişiklikleri bildirebilirsiniz.
