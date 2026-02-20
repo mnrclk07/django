e7c60d32fe69719eba6b73b4e2fa37c26be87f22
mnrclk07 <mnrclk21@gmail.com>
2026-02-20 18:43:38 +0300

    eleventh commit

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Movie
+from django.http import Http404
 
 # Create your views here.
 
@@ -11,8 +13,22 @@ def index(request):
     }
     return render(request, 'movies/list.html',context)
 
-def detail(request):
-    return render(request, 'movies/detail.html')
+def detail(request, movie_id):
+
+    # try:
+    #     movie = Movie.objects.get(pk  = movie_id)
+    # except Movie.DoesNotExist:
+    #     raise Http404('aradığınız kayıt yok')
+
+    # * or
+
+    movie = get_object_or_404(Movie, pk = movie_id)
+
+    context = {
+        'movie': movie
+    }
+
+    return render(request, 'movies/detail.html', context)
 
 def search(request):
     return render(request, 'movies/search.html')
